58af44f72bf836e257870aa90b6ee06aa49c1b3f
hhui64 <907322015@qq.com>
2023-03-26 19:34:08 +0800

    feat: dev config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 8081,
+    strictPort: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   plugins: [
     vue(),
     eslint({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue', 'src/*.ts', 'src/**/*.ts']
+      cache: true
     })
   ]
 })
