0a94f7b73a58f49e5c4c3064d94c50d222a1f0b7
hhui64 <907322015@qq.com>
2023-09-29 03:34:10 +0800

    perf: add tag attrs `lang="less"`

--- src/App.vue
+++ src/App.vue
@@ -19,19 +19,19 @@
   <HelloWorld msg="Vite + Vue + TS + ESLint" />
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-}
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  &.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
 }
 </style>
