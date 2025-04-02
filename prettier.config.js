4d563c093d7309b3bd542715af4f75033c028b39
hhui64 <907322015@qq.com>
2025-04-03 01:02:01 +0800

    chore: migrate Prettier configuration from .prettierrc.json to prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  trailingComma: 'none'
+}
+
+export default config
