c22e8c8d7f38e02cdd4f0fecb4501c7d67becf39
hhui64 <907322015@qq.com>
2025-08-30 02:18:04 +0800

    chore: add unplugin-vue-router for automatic route generation and update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes
 })
+
+// This will update routes at runtime without reloading the page
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueRouter from 'unplugin-vue-router/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,11 +15,14 @@ export default defineConfig({
     strictPort: true
   },
   plugins: [
+    vueRouter({
+      dts: 'src/types/typed-router.d.ts'
+    }),
     vue(),
     vueJsx(),
     AutoImport({
       dts: 'src/types/auto-imports.d.ts',
-      imports: ['vue', 'vue-router', 'pinia']
+      imports: ['vue', VueRouterAutoImports, 'pinia']
     }),
     Components({
       dts: 'src/types/components.d.ts',
